58fbb58f1e73880bf139ededc2c29d81d7422713
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-03-17 17:41:14 +0500

    feat: New files added

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+
+dotenv.config({path: path.resolve(__dirname, "../../.env.development")});
+
+
+
+
+module.exports = {
+  ENV: process.env.ENV,
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  URL: process.env.URL,
+};
---
+++ src/server.js
@@ -0,0 +1,31 @@
+const { meta } = require("@eslint/js");
+const config = require("./config/config");
+const express = require("express");
+
+const app = express();
+
+const server = app.listen(config.PORT);
+
+(() => {
+  try {
+    console.info(`Starting server...`, {
+      meta: {
+        PORT: config.PORT,
+        URL: config.URL,
+        DB_URL: config.DB_URL,
+      },
+    });
+  } catch (error) {
+    console.error(`Application is not running on port ${config.PORT}.`, {
+      meta: {
+        error: error.message,
+      },
+    });
+    server.close((error) => {
+      if (error) {
+        console.error(error);
+      }
+      process.exit(1);
+    });
+  }
+})();
